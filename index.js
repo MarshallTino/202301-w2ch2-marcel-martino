f3a006bde26b7c7c41988dbb06176b42848a6ef4
Marcel Martino <marcelmartino2053@gmail.com>
2023-01-18 08:07:30 +0100

    add console.log to the functions for each case.

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
-import strictEquals from "./strictEquals";
-console.log("hola");
-let valueA = 1;
-let valueB = 1;
-console.log(strictEquals(valueA, valueB));
+import strictEquals from "./strictEquals.js";
+
+console.log(strictEquals(1, 1) + "    1,1");
+console.log(strictEquals(NaN, NaN) + "   NaN,NaN");
+console.log(strictEquals(0, -0) + "    0,-0");
+console.log(strictEquals(-0, 0) + "    -0,0");
+console.log(strictEquals(1, "1") + "   1, '1'");
+console.log(strictEquals(true, false) + "   true,false");
+console.log(strictEquals(false, false) + "    false,true");
+console.log(strictEquals("water", "oil") + "   'water','oil'");
